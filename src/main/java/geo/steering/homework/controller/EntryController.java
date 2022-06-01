e09188ddc01bd306ae673c4449c03c24bda723c4
Roman Karagaev <rdkaragaev@gmail.com>
2022-06-01 09:34:39 +0700

    Update EntryController.java

--- src/main/java/geo/steering/homework/controller/EntryController.java
+++ src/main/java/geo/steering/homework/controller/EntryController.java
@@ -4,14 +4,11 @@ import geo.steering.homework.service.EntryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.Optional;
 
 @Slf4j
 @Controller
